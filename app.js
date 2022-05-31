cc8b768cb97a506ef6f696a687d2ae95f0d59af5
Isaiah Simon <60068759+IsaiahSimon@users.noreply.github.com>
2022-05-31 16:10:38 -0400

    Add feature: Render database items onto ToDo list
    - list.ejs:
    -- refactor for loop into a .forEach()
    -- access list item via its name property
    
    - app.js:
    -- nest insertMany control flow within Item.find() control flow
    -- update Post route from form to use MongoDB for rendering list items
    -- shift comments for readability

--- app.js
+++ app.js
@@ -2,27 +2,27 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
-//const date = require(__dirname + "/date.js");   // simplify for adding MongoDB
+//const date = require(__dirname + "/date.js");                             // simplify for adding MongoDB
 
 const app = express();
 
-app.set("view engine", "ejs"); // set up ejs for templating
+app.set("view engine", "ejs");                                              // set up ejs for templating
 
-app.use(bodyParser.urlencoded({ extended: true })); // parse form data client side
-app.use(express.static("public")); // set up static files folder
+app.use(bodyParser.urlencoded({ extended: true }));                         // parse form data client side
+app.use(express.static("public"));                                          // set up static files folder
 
 mongoose.connect("mongodb://localhost:27017/todolistDB", { useNewUrlParser: true }); // connect to mongodb
 
-const itemsSchema = new Schema({ // create schema for items
+const itemsSchema = new Schema({                                            // create schema for items
   name: String
 });
 
-const Item = mongoose.model(    // create model for items
-  "Item",                       // Singular name of collection
+const Item = mongoose.model(                                                // create model for items
+  "Item",                                                                   // Singular name of collection
   itemsSchema
 );
 
-const item1 = new Item({        // create new item
+const item1 = new Item({                                                    // create new item
   name: "Welcome to your todolist!"
 });
 
@@ -34,35 +34,41 @@ const item3 = new Item({
   name: "<-- Hit this to delete an item."
 });
 
-const defaultItems = [item1, item2, item3];   // create default items
-
-Item.insertMany(defaultItems, (err) => {      // insert default items
-  if (err) {
-    console.log(err);
-  } else {
-    console.log("Successfully saved default items to DB.");
-  }
-});
+const defaultItems = [item1, item2, item3];                                 // create default items
 
 // Home route
 app.get("/", (req, res) => {
-  // const day = date.getDate();  // simplify for adding MongoDB
-  // const day = date.getDay();   // optional: to use getDay() instead of getDate()
+  // const day = date.getDate();                                            // simplify for adding MongoDB
+  // const day = date.getDay();                                             // optional: to use getDay() instead of getDate()
 
-  res.render("list", { listTitle: "Today", newListItems: items }); // render the list.ejs file with all variables
+  Item.find({}, (err, foundItems) => {                                      // find all items in DB
+    //console.log(foundItems);
+    if (foundItems.length === 0) {                                          // if no items in DB, insert default items
+      Item.insertMany(defaultItems, (err) => {                              // insert default items
+        if (err) {
+          console.log(err);
+        } else {
+          console.log("Successfully added default items to DB.");
+        }
+      });
+      res.redirect("/");                                                    // redirect to home page
+    } else {
+      res.render("list", { listTitle: "Today", newListItems: foundItems }); // render the list.ejs file with all variables
+    }
+  });
 });
 
 // POST route from form
 app.post("/", (req, res) => {
-  const item = req.body.newItem; // get the new item from the form
+  const itemName = req.body.newItem;                                        // get the new item from the form
 
-  if (req.body.list === "Work") {
-    workItems.push(item);
-    res.redirect("/work");
-  } else {
-    items.push(item);
-    res.redirect("/");
-  }
+  const item = new Item({                                                   // create new item
+    name: itemName
+  });
+
+  item.save();                                                              // save item to DB
+
+  res.redirect("/");                                                        // redirect to home page
 });
 
 // Work route

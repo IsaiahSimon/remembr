ce12345fbec5e88e6b6bbd62fe066320172bc3bb
Isaiah Simon <60068759+IsaiahSimon@users.noreply.github.com>
2022-05-31 15:03:43 -0400

    Connect to MongoDB with Mongoose
    - disable date.js code to simplify integration
    - require Mongoose
    - connect Mongoose
    - add Schema, Model, and 3 Documents
    - create array to store Documents
    - insert Documents into array

--- app.js
+++ app.js
@@ -1,22 +1,55 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+//const date = require(__dirname + "/date.js");   // simplify for adding MongoDB
+
 const app = express();
-const date = require(__dirname + "/date.js");
 
 app.set("view engine", "ejs"); // set up ejs for templating
 
 app.use(bodyParser.urlencoded({ extended: true })); // parse form data client side
 app.use(express.static("public")); // set up static files folder
 
-const items = ["Buy Food", "Cook Food", "Eat Food"]; // outer scoped for all routes
-const workItems = [];
+mongoose.connect("mongodb://localhost:27017/todolistDB", { useNewUrlParser: true }); // connect to mongodb
+
+const itemsSchema = new Schema({ // create schema for items
+  name: String
+});
+
+const Item = mongoose.model(    // create model for items
+  "Item",                       // Singular name of collection
+  itemsSchema
+);
+
+const item1 = new Item({        // create new item
+  name: "Welcome to your todolist!"
+});
+
+const item2 = new Item({
+  name: "Hit the + button to add a new item."
+});
+
+const item3 = new Item({
+  name: "<-- Hit this to delete an item."
+});
+
+const defaultItems = [item1, item2, item3];   // create default items
+
+Item.insertMany(defaultItems, (err) => {      // insert default items
+  if (err) {
+    console.log(err);
+  } else {
+    console.log("Successfully saved default items to DB.");
+  }
+});
 
 // Home route
 app.get("/", (req, res) => {
-  const day = date.getDate();
+  // const day = date.getDate();  // simplify for adding MongoDB
   // const day = date.getDay();   // optional: to use getDay() instead of getDate()
 
-  res.render("list", { listTitle: day, newListItems: items }); // render the list.ejs file with all variables
+  res.render("list", { listTitle: "Today", newListItems: items }); // render the list.ejs file with all variables
 });
 
 // POST route from form
